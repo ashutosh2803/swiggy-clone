6740ac7ef8bb6c0f0b5b154350a75a0439c755eb
unknown <ashuisalive@gmail.com>
2024-08-04 23:23:56 +0530

    completed restaurant cards and what's on your mind

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://logosandtypes.com/wp-content/uploads/2021/01/Swiggy.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Swiggy Corporate</li>
+          <li>Search</li>
+          <li>Offers</li>
+          <li>Help</li>
+          <li>Sign In</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,11 @@
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <img alt="res-logo" src="" />
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
