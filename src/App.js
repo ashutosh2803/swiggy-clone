6de7908e2fc93c1e59139fbdf998ac69a0ca95f5
unknown <ashuisalive@gmail.com>
2024-08-08 01:07:48 +0530

    added routing along with error, about and contact pages placeholders with Restaurant Menu Page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import SearchBar from "./components/SearchBar";
 import WhatsOnYourMind from "./components/WhatsOnYourMind";
 import RestaurantCard from "./components/RestaurantCard";
 import Shimmer from "./components/Shimmer";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import RestaurantMenu from "./components/RestaurantMenu";
+import Error from "./components/Error";
 
 const Body = () => {
   const [restaurantData, setRestaurantData] = useState(resList);
@@ -140,16 +145,40 @@ const Body = () => {
     </div>
   );
 };
-
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1904,3 +1904,5 @@ export const resList = [
     subtype: "basic",
   },
 ];
+export const MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
